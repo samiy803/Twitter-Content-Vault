a65c8ab9dac9a7e220bb61f10cd880fc9948a04e
Sami Yousef <mail@samiyousef.ca>
2024-04-07 03:08:38 -0400

    new user

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Suspense, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
     Table,
     TableBody,
@@ -13,10 +13,25 @@ import {
 import { Button } from "@/components/ui/button";
 import { deleteUser } from "@/actions/delete";
 import { toast } from "sonner";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { newUser } from "@/actions/newUser";
+import { Checkbox } from "./ui/checkbox";
 
 export default function Users({ users }: { users: any[] }) {
     const [deleteId, setDeleteId] = useState<string | null>(null);
     const [userList, setUserList] = useState<any[]>(users);
+    const [newUserIsAdmin, setNewUserIsAdmin] = useState(false);
+    const [newUserIsOpen, setNewUserIsOpen] = useState(false);
 
     useEffect(() => {
         setUserList(users);
@@ -24,17 +39,34 @@ export default function Users({ users }: { users: any[] }) {
 
     function handleDelete(id: string) {
         if (deleteId === id) {
-            deleteUser(id).then(() => {
-                toast.success("User deleted.");
-                setDeleteId(null);
-                setUserList(userList.filter((u) => u.id !== id));
-            }).catch((e) => {
-                console.error(e);
-                toast.error(e.message);
-            })
+            deleteUser(id)
+                .then(() => {
+                    toast.success("User deleted.");
+                    setDeleteId(null);
+                    setUserList(userList.filter((u) => u.id !== id));
+                })
+                .catch((e) => {
+                    console.error(e);
+                    toast.error(e.message);
+                });
         } else {
             setDeleteId(id);
         }
+    }
+
+    function newUserWrapper(e: FormData) {
+        e.append("admin", newUserIsAdmin ? "true" : "false");
+        newUser(e)
+            .then((res) => {
+                console.log(res);
+                setUserList([...userList, res]);
+                setNewUserIsOpen(false);
+                toast.success("User created.");
+            })
+            .catch((e) => {
+                console.error(e);
+                toast.error(e.message);
+            });
     }
 
     return (
@@ -45,7 +77,78 @@ export default function Users({ users }: { users: any[] }) {
                         <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl font-extrabold my-8">
                             Users
                         </h1>
-                        <Button>New User</Button>
+                        <Dialog open={newUserIsOpen} onOpenChange={setNewUserIsOpen}>
+                            <DialogTrigger asChild>
+                                <Button>New User</Button>
+                            </DialogTrigger>
+                            <DialogContent className="sm:max-w-[425px]">
+                                <form action={newUserWrapper}>
+                                    <DialogHeader>
+                                        <DialogTitle>Add User</DialogTitle>
+                                        <DialogDescription>
+                                            Add a new user to the system.
+                                        </DialogDescription>
+                                    </DialogHeader>
+
+                                    <div className="grid gap-4 py-4">
+                                        <div className="grid grid-cols-4 items-center gap-4">
+                                            <Label
+                                                htmlFor="username"
+                                                className="text-right"
+                                            >
+                                                Username
+                                            </Label>
+                                            <Input
+                                                id="username"
+                                                name="username"
+                                                placeholder="sami yousef"
+                                                className="col-span-3"
+                                            />
+                                        </div>
+                                        <div className="grid grid-cols-4 items-center gap-4">
+                                            <Label
+                                                htmlFor="name"
+                                                className="text-right"
+                                            >
+                                                Password
+                                            </Label>
+                                            <Input
+                                                id="password"
+                                                placeholder="********"
+                                                className="col-span-3"
+                                                type="password"
+                                                name="password"
+                                            />
+                                        </div>
+                                        <div className="flex items-center space-x-2">
+                                            <Checkbox
+                                                id="admin"
+                                                checked={newUserIsAdmin}
+                                                onCheckedChange={(checked) =>
+                                                    setNewUserIsAdmin(
+                                                        checked ==
+                                                            "indeterminate"
+                                                            ? false
+                                                            : checked
+                                                    )
+                                                }
+                                            />
+                                            <label
+                                                htmlFor="admin"
+                                                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                            >
+                                                User is an admin?
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <DialogFooter>
+                                        <Button type="submit">
+                                            Save changes
+                                        </Button>
+                                    </DialogFooter>
+                                </form>
+                            </DialogContent>
+                        </Dialog>
                     </div>
                     <Table>
                         <TableCaption>
--- src/lib/pg.ts
+++ src/lib/pg.ts
@@ -1,58 +1,68 @@
-import { Sequelize, DataTypes } from 'sequelize';
+import { Sequelize, DataTypes } from "sequelize";
 
-
-export const sequelize = new Sequelize(process.env.PG_CONN || "");
-
-export const Joke = sequelize.define('Joke', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-    },
-    joke: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-    },
-    discarded: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: false,
-    },
-    used: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: false,
-    },
-    approved: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: false,
-    },
-}, {
-    createdAt: false,
-    updatedAt: false,
-    tableName: process.env.NODE_ENV == "production" ? "jokes" : "dev_jokes",
+export const sequelize = new Sequelize(process.env.PG_CONN || "", {
+    omitNull: true,
 });
 
-export const User = sequelize.define('User', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
+export const Joke = sequelize.define(
+    "Joke",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        joke: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        discarded: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
+        used: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
+        approved: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
     },
-    username: {
-        type: DataTypes.TEXT,
-        allowNull: false,
+    {
+        createdAt: false,
+        updatedAt: false,
+        tableName: process.env.NODE_ENV == "production" ? "jokes" : "dev_jokes",
+    }
+);
+
+export const User = sequelize.define(
+    "User",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+        },
+        username: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        admin: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
     },
-    password: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-    },
-    admin: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: false,
-    },
-}, {
-    createdAt: false,
-    updatedAt: false,
-    tableName: 'users',
-});
+    {
+        createdAt: false,
+        updatedAt: false,
+        tableName: "users",
+    }
+);
